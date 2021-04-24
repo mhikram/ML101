d8503fdcefff79884d5aab23049cb78e9a94c4d4
mhikram1@gmail.com <mhikram1@gmail.com>
2021-04-25 01:53:17 +0500

    ps1 complete

--- ps1/.auctex-auto/ps1.el
+++ ps1/.auctex-auto/ps1.el
@@ -5,13 +5,13 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -31,9 +31,11 @@
     "capt-of"
     "hyperref")
    (LaTeX-add-labels
-    "sec:org41b9195"
-    "sec:orgac692d1"
-    "sec:org22bb172"
-    "sec:orgf9068ff"))
+    "sec:org2564d6c"
+    "sec:org50a91f8"
+    "sec:org5894f23"
+    "sec:orgbb0d986"
+    "sec:org4b671dc"
+    "sec:org6f0a7df"))
  :latex)
 
